726778211754b3700fd47bee5f0e0623e209f8a5
jonatan <ramonjonatanvega@outlook.com>
2022-10-01 19:43:07 -0300

    TAREA (TASKLIST)

---
+++ README.md
@@ -0,0 +1,14 @@
+# TaskList 📝
+
+Este proyecto fue creado con Angular y es un task list de tareas el cual podemos agregar, quitar y marcar como realizadas.
+
+## Ejecución
+
+Para correr la aplicación web debemos:
+
+- Clonar el repositorio
+- Iniciar el deploy de angular tipeando en consola `npm start`.
+- Para ejecutar el server debemos también en consola tipear `json-server --watch db.json`
+<br></br><br></br>
+## 🙋‍♂️ Hola, Soy Jonatan Vega
+:nerd_face: Me encuentro cursando la carrera de fullstack developer 
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../tasks';
+import { TASKS } from 'src/app/mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+  
+  faTimes = faTimes;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
+}
